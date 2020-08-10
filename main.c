931d467434010e512731a29d70f2cb71e4987d25
pinguin007 <abdurahim.y34@gmail.com>
2020-08-10 02:42:01 +0000

    Added better comments. Defined direction variables. Deleted old versions

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
-/*#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
 
-
 int main(void) {
-  int keepPlaying=1;
-  int playerEntry;
+  
+  srand(time(NULL));//random time value used to randomize board
+  
+  
+  while(1){
+    int rows,cols;
 
-  while(keepPlaying){
-    int rows;
-    int cols;
-    while(rows<3||cols<3|| rows>8|| cols>8){
+    while(rows<3||cols<3|| rows>8|| cols>8){//repeats until the player enters rows and columns between 3-8
       printf("please enter the number of rows: \n");
       scanf("%d", &rows);
 
@@ -21,19 +21,18 @@ int main(void) {
       scanf("%d", &cols);
     }
     
-    int size= rows*cols;
-    int orderedBoard[size];
-    int gameBoard[size];
-    
-    for(int i=0; i <= size; i++){
-      orderedBoard[i]=i;
-    }
+    int size= rows*cols;//the overall size of the board
 
+    int gameBoard[size]; //intializing the gameBoard with all zeros
+		for (int i = 0; i < size; i++) {
+			gameBoard[i] = 0;
+		}
+    
+    // randomizing the location of the numbers  except the last spot. that's already a zero
     int j=0;
-    srand( time(NULL) );
     while(j < (size-1)){
           int flag=0;
-          int random= rand() % size;
+          int random= 1 + rand() % (size - 1);
           
           for(int i=0; i < size; i++){
             if(gameBoard[i]==random){
@@ -47,18 +46,25 @@ int main(void) {
           }
           
     }
-    gameBoard[size]=0;
 
-    orderedBoard[size-1]=0;
-
+    time_t start, end;		// Record the time taken in each round of play
+		time(&start);
+    int moveCounter=0;  //counts the number of moves
 
     int playerEntry=1;
 
-    while(playerEntry != 0){
-      
+    //main game loop. only broken when the player enters 0, or wins by putting the random table in order
+    while(playerEntry != 0){ 
+
+        system("clear"); //clear the screen to reprint the table with new moves
+
         int count=0;
-        char emptystring[2];
+        char emptystring[0];
         emptystring[0]='\0';
+
+        /*This for loop prints the array to the screen in a table fashion(rows, columns)
+        When the number of printed cells is perfectly divisable by the number of specified columns, we create a new row
+        It also prints an empty cell if it finds a zero 0*/
 
         for(int i=0; i < size; i++){
           
@@ -85,48 +91,89 @@ int main(void) {
           count++;
       }
 
-      printf("\nEnter the number you want to move(press '0' to exit): \n");
+      printf("\n(Move #%d)Enter the number you want to move(press '0' to exit):",moveCounter);
       scanf("%d",&playerEntry);
       
-      int num_location;
+      int num_location;//we find the cell in the array that contains the player's Entry
 
       for(int i=0; i < size; i++){
         if(gameBoard[i] == playerEntry){
           num_location= i;
-          //printf("\n%d\n", num_location);
         }   
       }
 
-      int zero_location;
+      int zero_location; //we find the cell in the array that contains the zero
 
       for(int i=0; i < size; i++){
         if(gameBoard[i] == 0){
           zero_location=i;
-          //printf("\n%d\n", zero_location);
         }
 
       }
 
-      if(zero_location + 1 == num_location || zero_location - 1 == num_location || zero_location + cols == num_location || zero_location - cols == num_location){
-        if ((zero_location+1) % cols == 0 && num_location == zero_location+1){
+      /*main game logic that checks the cells to the right of, the left of, above and below the empty cell.
+      If playerEntry is one of those cells, the playerEntry will be moved to the location of the empty cell.
+      The old location of the playerEntry becomes the new empty cell*/
+      int right = zero_location + 1;
+      int left = zero_location - 1;
+      int down = zero_location + cols;
+      int up = zero_location - cols;
+
+      if(right == num_location || left == num_location || down == num_location || up == num_location){
+        if ((zero_location+1) % cols == 0 && num_location == right){
           printf("\nNumber cannot be moved");
         }
-        else if(zero_location % cols == 0 && num_location == zero_location-1){
+        else if(zero_location % cols == 0 && num_location == left){
           printf("\nNumber cannot be moved");
         }
         else{
           gameBoard[zero_location] = gameBoard[num_location];
           gameBoard[num_location]=0;
+          
+          moveCounter++;
         }
       }
       else{
         printf("\nNumber cannot be moved");
       }
 
+      //cheat to solve the gameBoard
+      if(playerEntry==75949){
+        for(int i=1; i < size; i++){
+          gameBoard[i-1]=i;
+        }
+      }
+      
+      //checks if the board is organized by counting from the beginning and checking if the next number is greater by a factor of 1.
+      while(1){
+        int counter1=0;
+        for(int i=0; i<size;i++){
+          if (gameBoard[i]==counter1+1){
+            counter1++;
+          }
+        }
+        if (counter1==size-1){
+            double t = difftime(end,start);
+						printf("\nExcellent!!! you solved it with %d guesses, in %3.0f seconds.\n",moveCounter, t); 
+            playerEntry=0;
+        }
+        break;
+      }
+
     }
+
     printf("\ndo you want to keep playing. Yes(1), No(0): \n");
+
+    int keepPlaying; // exit the game if the player enters 0. loop to start new game if 1 is entered.
+
     scanf("%d", &keepPlaying);
-    }
+
+    if(keepPlaying == 0){break;}
   }
   
-*/
+  printf("GoodBye!\n");
+
+	return 0;
+}
+
+
